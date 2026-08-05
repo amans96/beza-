4133fa67809e262d773e1e35862fa230408bae36
amans96 <aman1996sisay@gmail.com>
2026-08-05 19:11:14 +0300

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import  Navbar from '../components/navbar.jsx'
 import navbarLinks from '../data/navbar.js';
 import AboutHero from "../components/AboutHero.jsx";
 import contact from "../assets/image1.jpg"
-import Map from '../components/map.jsx';
+import Map from '../components/Map.jsx';
 import ContactInfo from '../components/ContactInfo.jsx';
 import Footer from '../components/Footer.jsx';
 export default function Contact() {
